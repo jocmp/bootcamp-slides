2ce7772eb85918be318e2133990f228f73a920f2
Josiah Campbell <josiahcampbell64@gmail.com>
2017-05-15 11:47:40 -0400

    Add working svgs

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/index.html',
-  filename: 'index.html',
-  inject: 'body'
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
 });
 
 module.exports = {
@@ -19,25 +19,31 @@ module.exports = {
                 test: /\.tsx?$/,
                 loaders: ['ts-loader'],
                 exclude: /node_modules/,
-            }, 
-           { 
-            test: /\.scss$/,
-            use: [{
-                loader: "style-loader" // creates style nodes from JS strings
-            }, {
-                loader: "css-loader" // translates CSS into CommonJS
-            }, {
-                loader: "sass-loader" // compiles Sass to CSS
-            }]
-        },
+            },
             {
-                test: /\.svg$/,
-                loader: 'svg-inline-loader'
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: "url-loader",
+                    options: {
+                        limit: 10000,
+                        mimetype: "image/svg+xml"
+                    }
+                }]
             }
         ]
     },
     resolve: {
         extensions: [".tsx", ".ts", ".js"]
     },
-    plugins: [ HtmlWebpackPluginConfig ]
+    plugins: [HtmlWebpackPluginConfig]
 };
