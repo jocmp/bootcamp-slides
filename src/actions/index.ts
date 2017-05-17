a5c7c5d7553e1b28c5a39f2430facd2de68bfa08
Josiah Campbell <josiahcampbell64@gmail.com>
2017-05-17 09:19:47 -0400

    Add image option to two-column images

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -65,6 +65,14 @@ const apiData = (index: number): SlideshowModel => {
                     slide_type: "two-column",
                     content: ["By going into any level with a Fat Guy and carrying a Giant Yoshi Egg to the end of a level, the player must go to the edge of the screen past the exit and throw the egg up, then use Yoshi's tongue to push it slightly into the edge of the screen. By finishing the level like this, the next Yoshi will collect the egg as it appears to take Baby Mario, allowing the player to take a Giant Yoshi Egg into any level in the game.",
                     "Part of this game's \"character select\" theme is part of Yoshi's Island's \"Athletic\" theme. Shy Guys and Snifits also return in this game."]
+                },
+                {
+                    title: "Birds on Yoshi's Island",
+                    slide_type: "two-column",
+                    content: [
+                        "There are a few different bird species on Yoshi's Island. These include: Huffin Puffins and Goonies (pictured).",
+                        "https://i.imgur.com/SBaejMN.jpg"
+                    ]
                 }
             ]
         }
--- src/components/TwoColumnContent.tsx
+++ src/components/TwoColumnContent.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
 import { TwoColumnContentProps } from '../Props'
 
+const URL_MATCHER = new RegExp('(https?|ftp):\/\/[^\s/$.?#].[^\s]*');
+
+const isLink = (content: string): boolean => {
+    return URL_MATCHER.test(content);
+};
+
+const getContent = (content: string) => {
+    if (isLink(content)) {
+        return <img src={content} />
+    }
+    return <p>{content}</p>
+}
+
 const TwoColumnContent = (props: TwoColumnContentProps) => (
     <div className="slide-column">
-        {props.content}
+        {getContent(props.content)}
     </div>
 );
 
