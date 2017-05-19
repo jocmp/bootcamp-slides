5980b09b38281ea95ddca984999dade7b4e17a22
Josiah Campbell <josiahcampbell64@gmail.com>
2017-05-19 09:41:57 -0400

    Add google-esc homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,43 +4,39 @@ import { HomeProps } from '../Props';
 import { LinkFactory } from '../Models';
 import { Link } from 'react-router-dom';
 
-class Home extends React.Component<HomeProps, { selectedSlideshowId: string, timer: any }> {
+const logo = require('../assets/wordmark.png');
+
+class Home extends React.Component<HomeProps, { selectedSlideshowId: string }> {
 
     constructor(props: HomeProps) {
         super(props);
-        this.state = { selectedSlideshowId: '', timer: null };
+        this.state = { selectedSlideshowId: '' };
         console.log(this.props.slideshow);
     }
 
     handleChange = (event: any) => {
         this.props.clearError();
         this.setState({ selectedSlideshowId: event.target.value })
-        if (!!this.state.timer) {
-            clearTimeout(this.state.timer);
-        }
         if (!isNaN(Number(event.target.value))) {
             const searchValue = event.target.value;
-            const timer = setTimeout((() => {
-                this.props.searchSlideshows(Number(searchValue))
-            }), 200);
-            this.setState({ timer: timer });
+            this.props.searchSlideshows(Number(searchValue))
         }
     }
 
     render() {
         return (
             <div className="home-container">
-                <div className="item">
-                    <label>Slideshow ID</label>
-                    <input onChange={this.handleChange} value={this.state.selectedSlideshowId} type="text" />
-                    {this.props.slideshow.id > 0 &&
-                        <Link className="slide-button" to={LinkFactory.createSlideshowLink(this.props.slideshow.id)}>
-                            {this.props.slideshow.title}</Link>
-                    }
-                    {this.props.error.length > 0 && this.state.selectedSlideshowId &&
-                        <span>No Slideshows Found.</span>
-                    }
+                <div className="search-header">
+                    <img src={logo} className="search-logo" alt="logo" />
+                    <input placeholder="Search slideshow IDs" onChange={this.handleChange} value={this.state.selectedSlideshowId} type="text" />
                 </div>
+                <Link className={this.props.slideshow.id > 0 ? "slide-button" : "button-invisible"}
+                    to={LinkFactory.createSlideshowLink(this.props.slideshow.id)}>
+                    {this.props.slideshow.title}
+                </Link>
+                {this.props.error.length > 0 && this.state.selectedSlideshowId &&
+                    <span className="slide-error">No Slideshows Found.</span>
+                }
             </div>
         )
     }
